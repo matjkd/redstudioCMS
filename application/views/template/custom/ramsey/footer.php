794ca8a36e6b2ea58c03af8babbb0e37d3d4f8c4
mat sadler <mat@redstudio.co.uk>
2011-03-11 16:53:58 +0000

    alterations to footer and added google tracking code

--- application/views/template/custom/ramsey/footer.php
+++ application/views/template/custom/ramsey/footer.php
@@ -24,19 +24,7 @@
 
 
 
-  <!-- asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet 
-       change the UA-XXXXX-X to be your site's ID 
-  <script>
-   var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
-   (function(d, t) {
-    var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-    g.async = true;
-    g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g, s);
-   })(document, 'script');
-  </script>
-  -->
+  
    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=true&amp;key=<?=$maps_api?>" type="text/javascript"></script>
 
   <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script> 
--- application/views/template/custom/ramsey/sitefooter.php
+++ application/views/template/custom/ramsey/sitefooter.php
@@ -1,1 +1,2 @@
-<?=$config_address1?>, <?=$config_address2?>, <?=$config_address3?>, <?=$config_address4?>, <?=$config_address5?>
+<?=$config_company_name?>, <?=$config_address1?>, <?=$config_address2?>, <?=$config_address3?>, <?=$config_address4?>, <?=$config_address5?>. Tel: <?=$config_phone?>.
+ Website Developed by <a target="_blank"  href="http://www.redstudio.co.uk">Redstudio Design Limited</a>
