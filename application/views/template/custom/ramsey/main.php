a308e981328295330e1a4567e421d19a81ad9955
mat sadler <mat@redstudio.co.uk>
2011-03-11 13:10:19 +0000

    unhide custom themes

---
+++ application/views/template/custom/ramsey/main.php
@@ -0,0 +1,31 @@
+<!doctype html>  
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ --> 
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+ 
+<?=$this->load->view('template/custom/ramsey/header')?>
+
+</head>
+
+<body onload="initialize()" onunload="GUnload()">
+
+
+
+
+
+ 
+    <footer>
+footer
+    </footer>
+  </div> 
+
+<!--! end of #container -->
+<?=$this->load->view('template/custom/ramsey/footer')?>
+  
+</body>
+</html>
